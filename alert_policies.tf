a58b846b3b00efc2d67cf8b1e079ad9f80603895
Jeff Sushetski <jsushetski@gmail.com>
2022-03-06 23:08:26 -0700

    Disabled alert policy code

--- alert_policies.tf
+++ alert_policies.tf
@@ -1,40 +1,50 @@
-resource "google_monitoring_alert_policy" "alert_policies" {
-  provider = google
-
-  for_each = var.alert_policies
-
-  combiner = each.value.combiner == null ? var.alert_policy_defaults.combiner : each.value.combiner
-
-  display_name = each.value.display_name
-
-  # threshold conditions
-  dynamic "conditions" {
-    for_each = each.value.threshold_conditions
-    iterator = condition
-
-    content {
-      display_name = condition.value.display_name
-
-      condition_threshold {
-        comparison      = condition.value.comparison
-        duration        = condition.value.duration
-        filter          = condition.value.filter
-        threshold_value = condition.value.threshold_value
-
-        aggregations {
-          alignment_period     = condition.value.alignment_period
-          cross_series_reducer = condition.value.cross_series_reducer
-          group_by_fields      = condition.value.group_by_fields
-          per_series_aligner   = condition.value.per_series_aligner
-        }
-
-        trigger {
-          count   = condition.value.trigger.count
-          percent = condition.value.trigger.percent
-        }
-      }
-    }
-  }
-
-  notification_channels = each.value.notification_channels
-}
+#resource "google_monitoring_alert_policy" "alert_policies" {
+#  provider = google
+#
+#  for_each = var.alert_policies
+#
+#  combiner = each.value.combiner == null ? var.alert_policy_defaults.combiner : each.value.combiner
+#
+#  display_name = each.value.display_name
+#
+#  # threshold conditions
+#  dynamic "conditions" {
+#    for_each = each.value.threshold_conditions
+#    iterator = condition
+#
+#    content {
+#      display_name = condition.value.display_name
+#
+#      condition_threshold {
+#        comparison      = condition.value.comparison
+#        duration        = condition.value.duration
+#        filter          = condition.value.filter
+#        threshold_value = condition.value.threshold_value
+#
+#        aggregations {
+#          alignment_period     = condition.value.alignment_period
+#          cross_series_reducer = condition.value.cross_series_reducer
+#          group_by_fields      = condition.value.group_by_fields
+#          per_series_aligner   = condition.value.per_series_aligner
+#        }
+#
+#        trigger {
+#          count   = condition.value.trigger.count
+#          percent = condition.value.trigger.percent
+#        }
+#      }
+#    }
+#  }
+#
+#  # uptime check conditions
+#  dynamic "conditions" {
+#    for_each = each.value.uptime_checks
+#    iterator = condition
+#
+#    content {
+#      display_name = condition.value.display_name
+#    }
+#  }
+#
+#  notification_channels = each.value.notification_channels
+#}
--- variables.tf
+++ variables.tf
@@ -11,28 +11,16 @@ variable "alert_policy_defaults" {
   }
 }
 
-variable "alert_policies" {
-  type = list(object({
-    combiner     = optional(string)
-    display_name = string
-    threshold_conditions = list(object({
-      alignment_period     = string
-      comparison           = string
-      cross_series_reducer = string
-      display_name         = string
-      duration             = number
-      filter               = string
-      group_by_fields      = list(string)
-      per_series_aligner   = string
-      threshold_value      = number
-      trigger              = object({
-        count   = optional(number)
-        percent = optional(number)
-      })
-    }))
-    notification_channels = list(string)
-  }))
-}
+#variable "alert_policies" {
+#  type = list(object({
+#    combiner     = optional(string)
+#    display_name = string
+#    uptime_checks = list(object({
+#      display_name = string
+#    }))
+#    notification_channels = list(string)
+#  }))
+#}
 
 variable "notification_channels" {
   type = map(object({
