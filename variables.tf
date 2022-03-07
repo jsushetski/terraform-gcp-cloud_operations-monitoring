ce933cc2623b8cb5b099d9a252c8668bec1ffedb
Jeff Sushetski <jsushetski@gmail.com>
2022-03-06 22:50:49 -0700

    Started setting up alert policies

--- variables.tf
+++ variables.tf
@@ -2,6 +2,38 @@ variable "project" {
   type = string
 }
 
+variable "alert_policy_defaults" {
+  type = object({
+    combiner = string
+  })
+  default = {
+    combiner = "OR"
+  }
+}
+
+variable "alert_policies" {
+  type = map(object({
+    combiner     = optional(string)
+    display_name = string
+    threshold_conditions = list(object({
+      alignment_period     = string
+      comparison           = string
+      cross_series_reducer = string
+      display_name         = string
+      duration             = number
+      filter               = string
+      group_by_fields      = list(string)
+      per_series_aligner   = string
+      threshold_value      = number
+      trigger              = object({
+        count   = optional(number)
+        percent = optional(number)
+      })
+    }))
+    notification_channels = list(string)
+  }))
+}
+
 variable "notification_channels" {
   type = map(object({
     channel_type  = string
