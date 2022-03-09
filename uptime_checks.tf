9e24083b2d6a9469c3c1d35a2d9f415015709f22
Jeff Sushetski <jsushetski@gmail.com>
2022-03-08 22:41:02 -0700

    coalesce all the things!

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -4,18 +4,18 @@ resource "google_monitoring_uptime_check_config" "uptime_checks" {
   for_each = var.uptime_checks
 
   display_name     = each.key
-  period           = "${each.value.period == null ? var.uptime_check_defaults.period : each.value.period}s"
-  selected_regions = each.value.selected_regions == null ? var.uptime_check_defaults.selected_regions : each.value.selected_regions
-  timeout          = "${each.value.timeout == null ? var.uptime_check_defaults.timeout : each.value.timeout}s"
+  period           = "${coalesce(each.value.period, var.uptime_check_defaults.period)}s"
+  selected_regions = coalesce(each.value.selected_regions, var.uptime_check_defaults.selected_regions)
+  timeout          = "${coalesce(each.value.timeout, var.uptime_check_defaults.timeout)}s"
 
   dynamic "http_check" {
     for_each = each.value.http_check == null ? [] : [0]
 
     content { 
       path         = coalesce(each.value.http_check.path, var.uptime_check_defaults.http_check.path)
-      port         = each.value.http_check.port == null ? var.uptime_check_defaults.http_check.port : each.value.http_check.port
-      use_ssl      = each.value.http_check.use_ssl == null ? var.uptime_check_defaults.http_check.use_ssl : each.value.http_check.use_ssl
-      validate_ssl = (each.value.http_check.use_ssl == null ? var.uptime_check_defaults.http_check.use_ssl : each.value.http_check.use_ssl) ? (each.value.http_check.validate_ssl == null ? var.uptime_check_defaults.http_check.validate_ssl : each.value.http_check.validate_ssl) : null
+      port         = coalesce(each.value.http_check.port, var.uptime_check_defaults.http_check.port)
+      use_ssl      = coalesce(each.value.http_check.use_ssl == null, var.uptime_check_defaults.http_check.use_ssl)
+      validate_ssl = (coalesce(each.value.http_check.use_ssl, var.uptime_check_defaults.http_check.use_ssl) ? (coalesce(each.value.http_check.validate_ssl, var.uptime_check_defaults.http_check.validate_ssl) : null
       mask_headers = false
       headers      = {}
     }
